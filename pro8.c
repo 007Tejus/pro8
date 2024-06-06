d3ac65f09b83df41977dbd67a69536ce1877fa9e
007Tejus <161346725+007Tejus@users.noreply.github.com>
2024-06-06 07:28:44 +0530

    pro8.c

--- pro8.c
+++ pro8.c
@@ -1,162 +1,70 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+int a[500000];
 
-struct node
-{
-int page;
-struct node *left;
-struct node *right;
-};
-
-
-typedef struct node *NODEPTR;
-NODEPTR list=NULL;
-
-
-NODEPTR createlist(NODEPTR list,int page);
-NODEPTR moveforward(NODEPTR list, int cp ,int steps);
-NODEPTR movebackward(NODEPTR list, int cp, int steps);
-void display();
-NODEPTR getnode();
-
-NODEPTR createlist(NODEPTR list, int page)
-{
-	NODEPTR p,q;
-	p=getnode();
-	p->page=page;
-	p->left=NULL;
-	p->right=NULL;
-	if(list==NULL)
-		list=p;
-	else{
-		for(q=list; q->right!=NULL; q=q->right)
-		;
-		q->right=p;
-		p->left=q;
-
-	}
-	return(list);
-}
-
-	
-NODEPTR moveforward(NODEPTR list, int cp,int steps)
-{
-	NODEPTR p,q;
- 	int count=0,s;
-	if(list==NULL)
-		printf("\n Empty list");
-	else{
-		p=list;
-                for(p=list,count=0;count<cp-1; count++)
-                  p=p->right;
-             
-                for(q=p,s=0;s<steps;s++)
-                 {
-                    if(q->right==NULL)
-		    	return(q);
-		    q=q->right;
- 		   }
-                 return(q);
-}
-}
-
-NODEPTR movebackward(NODEPTR list, int cp,int steps)
-{
-	NODEPTR p,q;
- 	int count=0,s;
-	if(list==NULL)
-		printf("\n Empty list");
-	else{
-		p=list;
-                for(p=list,count=0;count<cp-1; count++)
-                  p=p->right;
-             
-                for(q=p,s=0;s<steps;s++)
-                 {
-                    if(q->left==NULL)
-		            	return(q);
-		           q=q->left;
- 		   }
-                 return(q);
+void merge(int low, int mid, int high);
+void merge_sort(int low, int high) {
+    int mid;
+    if (low < high) {
+        mid = (low + high) / 2;
+        merge_sort(low, mid);
+        merge_sort(mid + 1, high);
+        merge(low, mid, high);
     }
 }
 
-
-void display(NODEPTR list)
-{
-	NODEPTR p;
-	p=list;
-	if(p==NULL)
-		printf("\nEmpty list");
-	else
-	{
-		printf("\n The page list contains: ");
-		for(p=list;p!=NULL;p=p->right) 
-		{
-			printf("%d<->",p->page);
-		}
-	}
+void merge(int low, int mid, int high) {
+    int h, i, j, b[500000], k;
+    h = low;
+    i = low;
+    j = mid + 1;
+    while ((h <= mid) && (j <= high)) {
+        if (a[h] <= a[j]) {
+            b[i] = a[h];
+            h++;
+        } else {
+            b[i] = a[j];
+            j++;
+        }
+        i++;
+    }
+    if (h > mid) {
+        for (k = j; k <= high; k++) {
+            b[i] = a[k];
+            i++;
+        }
+    } else {
+        for (k = h; k <= mid; k++) {
+            b[i] = a[k];
+            i++;
+        }
+    }
+    for (k = low; k <= high; k++) {
+        a[k] = b[k];
+    }
 }
 
-NODEPTR getnode()
-{
-	NODEPTR r;
-	r=(NODEPTR)malloc(sizeof(struct node));
-	if(r==NULL)
-	{
-		printf("\n Node allocation failed");
-		exit(0);
-	}
-	return(r);
-}
-
-
-void main()
-{
-		int page,choice,steps,cp;
- 		int cont;
-		NODEPTR p;
-		do{
-		printf("\n ...........MENU...........");
-		printf("\n 1->CREATE LIST\t  2->MOVE FORWARD\t  3->MOVE BACKWARD\t 4->DISPLAY 5->EXIT");
-		printf("\n Enter your choice");
-		scanf("%d",&choice);
-		switch(choice)
-		{
-		case 1:printf("\n CREATION OF DOUBLY LINKED LIST OF PAGES IS IN PROGRESS:\n");
-			do{
-              	        	  printf("Enter a page number:");
-                        	  scanf("%d",&page);
-              	 	 	  list = createlist(list,page);
-              	 		  printf("Do you want to enter another page[1/0]:");
-               	 		  scanf(" %d",&cont);
-              		 }while(cont==1);
-               		  display(list); 
-			  break;
-		 	
-		case 2: printf("\n MOVE FORWARD:\n");
-			printf("\nEnter the current page:");
-                        scanf("%d",&cp);
-			printf("Enter the number of steps to move forward");
-			scanf("%d",&steps);
-			p=moveforward(list,cp,steps);
-			printf("\n Moved forward to  page %d from %dth page",p->page, cp);
-			break;
-		case 3: printf("\n MOVE BACKWARD:\n");
-			printf("\nEnter the current page:");
-                        scanf("%d",&cp);
-			printf("Enter the number of steps to move backward");
-			scanf("%d",&steps);
-			p=movebackward(list,cp,steps);
-			printf("\n Moved backwards to  page %d from %dth page",p->page, cp);
-			break;
-		case 4: display(list);
-		        break;
-		case 5:printf("\n Quitting operation List.....\n");
-			break;
-		default:printf("\n Invalid choice");
-			break;
-		}		
-	}while(choice!=5);
+int main() {
+    int n, i;
+    double clk;
+    clock_t starttime, endtime;
+    printf("MERGE SORT\n");
+    printf("Enter the number of employee records:\n ");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) {
+        a[i] = rand() % 100;
+        printf("The Employee IDs are: \t%d\n", a[i]);
+    }
+    starttime = clock();
+    merge_sort(0, n - 1);
+    endtime = clock();
+    clk = (double)(endtime - starttime) / CLOCKS_PER_SEC;
+    printf("\nEmployee IDs in sorted order:\n");
+    for (i = 0; i < n; i++) {
+        printf("\t%d\n", a[i]);
+    }
+    printf("\nThe run time is %f\n", clk);
+    return 0;
 }
